b2450343fd9a2fe33102180ca567e95ab77cbd4a
iiiiio <y.zhihon@gmail.com>
2019-07-13 22:05:43 +0800

    logging enabled

--- superlists/settings.py
+++ superlists/settings.py
@@ -132,3 +132,20 @@ EMAIL_HOST_USER = 'forctrl996@gmail.com'
 EMAIL_HOST_PASSWORD = 'cormovan7766' #os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
